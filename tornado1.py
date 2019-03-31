015d93bae5597e75d2cc1e2fc06aa0ce8c1dd779
tg123456 <2947801709@qq.com>
2019-03-31 15:25:35 +0800

    Merge branch 'tanggang9' into tg9
    # Conflicts:
    #	.idea/workspace.xml
    #	tornado1.py

--- tornado1.py
+++ tornado1.py
@@ -4,7 +4,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        print("tg9")
+        print("tg9123S")
         print("tanggang9")
         self.write("Hello world")
 
