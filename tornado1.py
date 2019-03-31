0f4b7fb6ae041b6d4760e656e8573959d3c7dcc1
tg123456 <2947801709@qq.com>
2019-03-31 14:26:51 +0800

    master

---
+++ tornado1.py
@@ -0,0 +1,31 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world")
+
+
+class StoryHandler(tornado.web.RequestHandler):
+    def get(self, story_id):
+        self.write("You requested the story " + story_id)
+
+
+class BuyHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("buy.wupeiqi.com/index")
+
+
+application = tornado.web.Application([
+    (r"/index", MainHandler),
+    (r"/story/([0-9])",StoryHandler),
+])
+
+application.add_handlers("buy.wupeiqi.com$",[
+    (r"/index",BuyHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
